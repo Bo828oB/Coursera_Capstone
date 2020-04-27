37cd861eb26897781bd157feeeaacacc930615c7
Sun Bo <sunbo@SundeMacBook-Pro.local>
2020-04-27 17:21:19 +0200

    import packages and print

---
+++ Hi_Capstone.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # First Coursera Project
+
+# ### This is for Coursera project, as the name of project. Hope I can achieve it and enjoy it. Feel free to try.
+# #### In this project one task is to create an account on Github
+# #### then a few lines of code in Python
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
